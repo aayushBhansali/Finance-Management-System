173d95a8da893d0ce1b78964a756ac599850ae27
aayushBhansali <aayushbhansali692@gmail.com>
2019-08-01 10:12:22 +0530

    Data Stored and Written into Database

---
+++ Finance.py
@@ -0,0 +1,48 @@
+import sqlite3 as sql
+from datetime import date
+
+class Finance:
+
+    def __init__(self):
+        self.date = date.today().strftime("%d-%m-%y")
+        self.expense_name = "None"
+        self.expense_amount = 0
+        self.income_name = "None"
+        self.income_amount = 0
+
+
+    def showFinance(self):
+        print("Date : " + str(self.date))
+        print("Income : " + str(self.income_name))
+        print("Amount : " + str(self.income_amount))
+        print("Expense : " + str(self.expense_name))
+        print("Amount : " + str(self.expense_amount))
+
+
+    def add_expense(self, name, amount):
+        self.expense_name = name
+        self.expense_amount = amount
+
+
+    def add_income(self, name, amount):
+        self.income_name = name
+        self.income_amount = amount
+
+
+    def write_to_db(self):
+        db = sql.connect("Finance.db")
+        cur = db.cursor()
+        cur.execute("INSERT INTO Finance (Date, Expense_name, Expense_amount, Income_name, Income_amount) VALUES (" + "'" + str(self.date) + "', '" + str(self.expense_name) + "', " + str(self.expense_amount) + ", '" + str(self.income_name) + "', " + str(self.income_amount) + " )")
+        db.commit()
+        db.close()
+
+def procedure():
+    f = Finance()
+    f.showFinance()
+    f.add_expense("Fruits", 100)
+    f.add_income("Pocket", 300)
+    f.write_to_db()
+
+
+if __name__ == "__main__":
+    procedure()

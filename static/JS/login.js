54bcd6ec0822b7cd1f7a4951f525b8bf118e1d5a
aayushBhansali <aayushbhansali692@gmail.com>
2019-11-20 11:00:21 +0530

    Added Validation to Login Page

--- static/JS/login.js
+++ static/JS/login.js
@@ -1,5 +1,20 @@
-login-success = function(){
+let login_success = function(){
   document.getElementByID("login-btn").onclick = function(){
       location.href = "http://127.0.0.1:5000/signup";
     };
 }
+
+
+let validate = function(){
+  let uname = document.getElementById("usname").value;
+  let passwd = document.getElementById("passwd").value;
+  let form = document.getElementById("form");
+
+  if (uname === "" || passwd === ""){
+    alert("Please Enter A Valid Username and Password");
+    form.method = "get";
+  } else{
+    form.action = "http://127.0.0.1:5000/login-success";
+  }
+}
+
